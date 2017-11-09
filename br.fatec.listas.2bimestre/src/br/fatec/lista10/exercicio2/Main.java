04d3e5b7f43dfe46c57267fbbf1bf2aebd7e6ab4
ViniciusLelis <Admin@SERVTIMERCAFE>
2017-11-09 16:17:29 -0200

    09-11-2017-16:17

--- br.fatec.listas.2bimestre/src/br/fatec/lista10/exercicio2/Main.java
+++ br.fatec.listas.2bimestre/src/br/fatec/lista10/exercicio2/Main.java
@@ -13,35 +13,35 @@ import static javax.swing.JOptionPane.OK_OPTION;
  * @author Admin
  */
 public class Main {
-
     private int Quantidade;
     
         public static void main(String[] args) {
           new Main();
     }
+        
         Main(){
-        boolean opcao = false;
-            while(!opcao) {
-            String texto = JOptionPane.showInputDialog(null, "Digite o texto desejado: ");
-            contar(texto);
-            int result = JOptionPane.showConfirmDialog(null, "Deseja verificar mais um texto?", "Pergunta", OK_OPTION);
-                if(result== OK_OPTION)
-                    opcao = false;
-                else
-                    opcao = true;
-            }
+            boolean opcao = false;
+                while(!opcao) {
+                String texto = JOptionPane.showInputDialog(null, "Digite o texto desejado: ");
+                contar(texto);
+                int result = JOptionPane.showConfirmDialog(null, "Deseja verificar mais um texto?", "Pergunta", OK_OPTION);
+                    if(result== OK_OPTION)
+                        opcao = false;
+                    else
+                        opcao = true;
+                }
         }
         
         public void contar(String texto) {
             for(int i=0; i<texto.length();i++){  
 	       char letra = texto.toUpperCase().charAt(i);  
-	       String x1 = String.valueOf(letra);  
-	       if(x1.equalsIgnoreCase("A") || x1.equalsIgnoreCase("D") || x1.equalsIgnoreCase("O") || x1.equalsIgnoreCase("Q") ||
-                       x1.equalsIgnoreCase("R")){ 
+	       String aa = String.valueOf(letra);  
+	       if(aa.equalsIgnoreCase("A") || aa.equalsIgnoreCase("D") || aa.equalsIgnoreCase("O") || aa.equalsIgnoreCase("Q") ||
+                       aa.equalsIgnoreCase("R")){ 
                    
 		    Quantidade += 1;  
 		    }
-               else if(x1.equalsIgnoreCase("B")){
+               else if(aa.equalsIgnoreCase("B")){
                    Quantidade += 2;
                }
             }
